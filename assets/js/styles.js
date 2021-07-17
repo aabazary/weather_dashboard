b73ef0d36634d9aa5ba2a74a7d84591a580353ad
aabazary <aabazary@gmail.com>
2021-07-17 16:59:57 -0700

    added most of the function of the weather card, working on UVI

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,39 +1,62 @@
 var key = "c5977d806189278697c81338ef7cc9fd";
 var searchBtn = document.getElementById('btn');
-var cityName = document.getElementById('city-name');
-var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + key;
+var cityInput = document.getElementById('cityInput');
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + key;
+var temperature = document.getElementById('temperature');
+var cityHeader = document.getElementById('cityName');
+var date = document.getElementById('date');
+var temperature = document.getElementById('temperature');
+var humidity = document.getElementById('humidity');
+var windSpeed = document.getElementById('windSpeed')
+var uvIndex = document.getElementById('uvIndex')
+var weatherImg = document.getElementById('weatherImg')
 
 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    cityName + "&units=imperial&appid=" + key;
+    cityInput + "&units=imperial&appid=" + key;
 
 function getWeather() {
-    var cityName = document.getElementById('city-name').value.trim()
-    .replace(' ', '+');
+    var cityInput = document.getElementById('cityInput').value.trim()
+        .replace(' ', '+');
     var key = 'c5977d806189278697c81338ef7cc9fd';
 
-    if (!cityName) {
+    if (!cityInput) {
         alert('You need a search input value!');
         return;
-    } 
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + key;
+    }
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + key;
 
-    var forecastUrl =
-        'https://api.openweathermap.org/data/2.5/forecast?q=' +
-        cityName +
-        '&units=imperial&appid=' +
-        key;
+    var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&units=imperial&appid=' + key;
     console.log(weatherUrl);
     console.log(forecastUrl);
 
+
     fetch(weatherUrl)
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.name)
+            cityHeader.innerText = data.name;
+            weatherImg.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+            temperature.innerText = "Temperature: " + data.main.temp;
+            humidity.innerText = "Humidity: " + data.main.humidity;
+            windSpeed.innerText = "Wind Speed " + data.wind.speed;
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            var uvUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&cnt=1";
+        
+            fetch(uvUrl)
+                .then(res => res.json())
+                .then(res => {
+                console.log(uvUrl)
+                console.log(res.value)
+    })
+
+        })
+    
 
     fetch(forecastUrl)
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
-
 
 searchBtn.addEventListener('click', getWeather);
 
